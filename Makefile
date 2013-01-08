469707f50d7574f82991f2d911e3624c98a516e0
FND <fnd@localhost.localdomain>
2013-01-08 18:27:04 +0100

    initial commit
    basic infrastructure

---
+++ Makefile
@@ -0,0 +1,11 @@
+.PHONY: readme clean
+
+readme:
+	python -c "import mangler; import tiddlywebplugins.gitstore as pkg; \
+			print pkg.__doc__.strip()" > README
+
+clean:
+	find . -name "*.pyc" | xargs rm || true
+	rm -rf html .figleaf coverage.lst # figleaf
+	rm -rf htmlcov .coverage # coverage
+	rm -rf test/__pycache__ # pytest
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+DESC = open(os.path.join(os.path.dirname(__file__), 'README')).read()
+
+META = {
+    #'name': '',
+    #'url': 'https://github.com/FND/',
+    #'version': VERSION,
+    #'description': '',
+    'long_description': DESC,
+    #'license': 'LICENSE',
+    #'author': AUTHOR,
+    #'author_email': '',
+    'maintainer': 'FND',
+    'packages': find_packages(exclude=['test']),
+    'platforms': 'Posix; MacOS X; Windows',
+    'include_package_data': False,
+    'zip_safe': False,
+    'install_requires': ['tiddlyweb', 'tiddlywebplugins.utils'],
+    'extras_require': {
+        'testing': ['pytest'],
+        'coverage': ['figleaf', 'coverage']
+    }
+}
+
+
+#setup(**META)
