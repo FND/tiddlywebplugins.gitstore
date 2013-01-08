121e8ba35238eb05b6a8a52f7883b2bcf9ba7d5d
FND <fnd@localhost.localdomain>
2013-01-08 21:42:51 +0100

    ensured existing stores are handled properly
    no test because that's kinda cumbersome to do, it's late and I'm lazy

--- tiddlywebplugins/gitstore/__init__.py
+++ tiddlywebplugins/gitstore/__init__.py
@@ -1,10 +1,11 @@
 """
 Git Store
 TiddlyWeb store implementation using Git - based on the default text store, this
-store uses Git to keep track of tiddler revisions.
+store uses Git to keep track of tiddler revisions
 """
 
 from dulwich.repo import Repo
+from dulwich.errors import NotGitRepository
 
 from tiddlyweb.stores.text import Store as TextStore
 
@@ -13,4 +14,7 @@ class Store(TextStore):
 
     def __init__(self, store_config=None, environ=None):
         super(Store, self).__init__(store_config, environ)
-        Repo.init(self._root)
+        try:
+            self.repo = Repo(self._root)
+        except NotGitRepository:
+            self.repo = Repo.init(self._root)
