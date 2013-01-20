664e0a2f2b40c0500690e4cbfcdf0c514e852594
FND <fnd@localhost.localdomain>
2013-01-20 15:39:36 +0100

    attempt to access serialization directly
    for debugging purposes; this, too, will raise a StoreLockError

--- tiddlywebplugins/gitstore/__init__.py
+++ tiddlywebplugins/gitstore/__init__.py
@@ -67,7 +67,10 @@ class Store(TextStore):
             modifier = tiddler.modifier
             modified = tiddler.modified
 
-            self.serializer.object = Tiddler()
+            contents = read_utf8_file(tiddler_filename)
+            previous_revision = Tiddler(tiddler.title, tiddler.bag)
+            previous_revision = self.serializer.serialization. \
+                    as_tiddler(previous_revision, contents)
 
             # restore fields
             tiddler.modifier = modifier
